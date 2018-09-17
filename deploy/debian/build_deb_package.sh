82def1851b3776fa53fa9a4bcf720fb1f7b98cb1
Denis Kormalev <denis.kormalev@opensoftdev.com>
2018-09-17 15:16:22 -0700

    Respecting down file absence on debian package upgrading

--- deploy/debian/build_deb_package.sh
+++ deploy/debian/build_deb_package.sh
@@ -159,6 +159,9 @@ PROOF_VERSION=`$PROOF_PATH/dev-tools/travis/grep_proof_version.sh $PROOF_PATH`
 # Building package
 mkdir -p "$PACKAGE_ROOT/DEBIAN"
 cp -R "$ROOT"/DEBIAN/* "$PACKAGE_ROOT"/DEBIAN/
+sed -E "s|##APP_RESTARTER_PATH##|/opt/Opensoft/proof-restarter/$PACKAGE_NAME|" -i "$PACKAGE_ROOT"/DEBIAN/preinst
+sed -E "s|##APP_RESTARTER_PATH##|/opt/Opensoft/proof-restarter/$PACKAGE_NAME|" -i "$PACKAGE_ROOT"/DEBIAN/postinst
+
 cat << EOT > "$PACKAGE_ROOT/DEBIAN/control"
 Package: $PACKAGE_NAME
 Version: $VERSION
