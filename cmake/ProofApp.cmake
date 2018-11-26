7179d39e3531468ce5f0699d519d2a733bb50fe4
Denis Kormalev <denis.kormalev@opensoftdev.com>
2018-11-26 15:56:00 -0800

    app tests suport in cmake builds

--- cmake/ProofApp.cmake
+++ cmake/ProofApp.cmake
@@ -14,7 +14,10 @@ macro(proof_project project_name)
     include(ProofAndroidApk OPTIONAL)
     add_compile_definitions(APP_VERSION=\"${_arg_VERSION}\")
     include_directories(${CMAKE_CURRENT_SOURCE_DIR})
+    include_directories(${CMAKE_CURRENT_SOURCE_DIR}/src)
     list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/cmake")
+    include(GoogleTest)
+    enable_testing()
 endmacro()
 
 function(proof_add_app target)
@@ -93,4 +96,53 @@ endfunction()
 
 function(proof_add_service target)
     proof_add_app(${target} ${ARGN} AUTOSTART)
+endfunction()
+
+function(proof_add_app_test target)
+    if (PROOF_SKIP_TESTS)
+        return()
+    endif()
+    find_package(proof-gtest CONFIG REQUIRED)
+
+    cmake_parse_arguments(_arg
+        ""
+        ""
+        "QT_LIBS;PROOF_LIBS;OTHER_LIBS"
+        ${ARGN}
+    )
+
+    foreach(QT_LIB ${_arg_QT_LIBS})
+        find_package("Qt5${QT_LIB}" CONFIG REQUIRED)
+        set(QT_LIBS ${QT_LIBS} "Qt5::${QT_LIB}")
+    endforeach()
+
+    foreach(PROOF_LIB ${_arg_PROOF_LIBS})
+        find_package("Proof${PROOF_LIB}" CONFIG REQUIRED)
+        set(PROOF_LIBS ${PROOF_LIBS} "Proof::${PROOF_LIB}")
+    endforeach()
+
+    if (EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/tests/main.cpp)
+        set(sources_copy ${Proof_${target}_SOURCES} "")
+        list(FILTER sources_copy INCLUDE REGEX ".*main.cpp$")
+        list(LENGTH sources_copy main_found)
+        if (main_found EQUAL 0)
+            set(Proof_${target}_SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/tests/main.cpp ${Proof_${target}_SOURCES})
+        endif()
+    endif()
+
+    add_executable(${target}
+        ${Proof_${target}_SOURCES} ${Proof_${target}_RESOURCES}
+        ${Proof_${target}_PUBLIC_HEADERS}
+        ${Proof_${target}_MOC_SOURCES}
+    )
+
+    proof_set_cxx_target_properties(${target})
+    target_link_libraries(${target} ${QT_LIBS} ${PROOF_LIBS} ${_arg_OTHER_LIBS} proof-gtest)
+
+    if ((NOT ANDROID) AND (NOT PROOF_SKIP_CTEST_TARGETS))
+        gtest_discover_tests(${target}
+            DISCOVERY_TIMEOUT 30
+            PROPERTIES TIMEOUT 30
+        )
+    endif()
 endfunction()
