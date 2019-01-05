c69d06955bbffec3a5d4743c5cbc60b6202e5b05
Denis Kormalev <denis.kormalev@opensoftdev.com>
2019-01-04 16:25:08 -0800

    New ndk location in travis android docker image [AB#186378]

--- travis/proof/compile/android_compile.sh
+++ travis/proof/compile/android_compile.sh
@@ -51,20 +51,18 @@ echo " ";
 
 travis_fold start "prepare.android_ndk" && travis_time_start;
 echo -e "\033[1;33mPreparing Android NDK...\033[0m";
-echo "$ mv /ndk-bundle.tar.xz /opt/android/sdk/ndk-bundle.tar.xz";
-docker exec -t builder sh -c "mv /ndk-bundle.tar.xz /opt/android/sdk/ndk-bundle.tar.xz";
-echo "$ cd /opt/android/sdk && tar -xJf ndk-bundle.tar.xz";
-docker exec -t builder sh -c "cd /opt/android/sdk && tar -xJf ndk-bundle.tar.xz";
-echo "$ rm /opt/android/sdk/ndk-bundle.tar.xz";
-docker exec -t builder sh -c "rm /opt/android/sdk/ndk-bundle.tar.xz";
+echo "$ mv /ndk.tar.gz /opt/android/ndk.tar.gz";
+docker exec -t builder sh -c "mv /ndk.tar.gz /opt/android/ndk.tar.gz";
+echo "$ cd /opt/android && tar -xzf ndk.tar.gz";
+docker exec -t builder sh -c "cd /opt/android && tar -xzf ndk.tar.gz";
 travis_time_finish && travis_fold end "prepare.android_ndk";
 echo " ";
 
 travis_fold start "build.cmake" && travis_time_start;
 echo -e "\033[1;33mRunning cmake...\033[0m";
-echo "$ cmake -DANDROID_PLATFORM=android-16 -DANDROID_STL=c++_shared -DNDK_CCACHE=$CCACHE_CMD -DCMAKE_BUILD_TYPE=Release -DCMAKE_FIND_ROOT_PATH=\$QTDIR -DCMAKE_INSTALL_PREFIX=/sandbox/bin -DPROOF_CI_BUILD:BOOL=ON -DCMAKE_TOOLCHAIN_FILE=\$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake -G 'Unix Makefiles' ../proof";
+echo "$ cmake -DANDROID_PLATFORM=android-28 -DANDROID_STL=c++_shared -DNDK_CCACHE=$CCACHE_CMD -DCMAKE_BUILD_TYPE=Release -DCMAKE_FIND_ROOT_PATH=\$QTDIR -DCMAKE_INSTALL_PREFIX=/sandbox/bin -DPROOF_CI_BUILD:BOOL=ON -DCMAKE_TOOLCHAIN_FILE=\$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake -G 'Unix Makefiles' ../proof";
 docker exec -t builder bash -c "exec 3>&1; set -o pipefail; rm -rf /sandbox/logs/*; mkdir build && cd build; \
-    cmake -DANDROID_PLATFORM=android-16 -DANDROID_STL=c++_shared -DNDK_CCACHE=$CCACHE_CMD \
+    cmake -DANDROID_PLATFORM=android-28 -DANDROID_STL=c++_shared -DNDK_CCACHE=$CCACHE_CMD \
         -DCMAKE_BUILD_TYPE=Release -DCMAKE_FIND_ROOT_PATH=\$QTDIR -DCMAKE_INSTALL_PREFIX=/sandbox/bin -DPROOF_CI_BUILD:BOOL=ON \
         -DCMAKE_TOOLCHAIN_FILE=\$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake -G 'Unix Makefiles' \
         ../proof 2>&1 1>&3 | (tee /sandbox/logs/errors.log 1>&2)";
