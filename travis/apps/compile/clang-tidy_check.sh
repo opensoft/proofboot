6634267cfa894fdfab8e63225a456fcd964f7e37
Denis Kormalev <denis.kormalev@opensoftdev.com>
2019-06-21 11:50:46 -0700

    jdk includes added to clang-tidy travis jobs

--- travis/apps/compile/clang-tidy_check.sh
+++ travis/apps/compile/clang-tidy_check.sh
@@ -66,10 +66,10 @@ fi
 
 travis_fold start "build.cmake" && travis_time_start;
 echo -e "\033[1;33mRunning cmake...\033[0m";
-echo "$ cmake -DCMAKE_BUILD_TYPE=Debug '-DCMAKE_CXX_FLAGS=-ferror-limit=0 -fcolor-diagnostics -stdlib=libc++ -isystem /usr/lib/llvm-7/include/c++/v1' -DPROOF_CLANG_TIDY=ON -DPROOF_SKIP_TESTS:BOOL=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \"-DCMAKE_PREFIX_PATH=/opt/Opensoft/Qt;/sandbox/proof-bin\" -G 'Unix Makefiles' ../target_src";
+echo "$ cmake -DCMAKE_BUILD_TYPE=Debug '-DCMAKE_CXX_FLAGS=-ferror-limit=0 -fcolor-diagnostics -stdlib=libc++ -isystem /usr/lib/llvm-7/include/c++/v1 -isystem /usr/lib/jvm/java-8-openjdk-amd64/include -isystem /usr/lib/jvm/java-8-openjdk-amd64/include/linux -isystem /sandbox/non_tidy' -DPROOF_CLANG_TIDY=ON -DPROOF_SKIP_TESTS:BOOL=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \"-DCMAKE_PREFIX_PATH=/opt/Opensoft/Qt;/sandbox/proof-bin\" -G 'Unix Makefiles' ../target_src";
 docker exec -t builder bash -c "exec 3>&1; set -o pipefail; rm -rf /sandbox/logs/*; mkdir build && cd build; \
     cmake -DCMAKE_BUILD_TYPE=Debug \
-        '-DCMAKE_CXX_FLAGS=-ferror-limit=0 -fcolor-diagnostics -stdlib=libc++ -isystem /usr/lib/llvm-7/include/c++/v1 -isystem /sandbox/non_tidy' \
+        '-DCMAKE_CXX_FLAGS=-ferror-limit=0 -fcolor-diagnostics -stdlib=libc++ -isystem /usr/lib/llvm-7/include/c++/v1 -isystem /usr/lib/jvm/java-8-openjdk-amd64/include -isystem /usr/lib/jvm/java-8-openjdk-amd64/include/linux -isystem /sandbox/non_tidy' \
         -DPROOF_CLANG_TIDY=ON -DPROOF_SKIP_TESTS:BOOL=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
         \"-DCMAKE_PREFIX_PATH=/opt/Opensoft/Qt;/sandbox/proof-bin\" -G 'Unix Makefiles' \
         ../target_src 2>&1 1>&3 | (tee /sandbox/logs/errors.log 1>&2)";
