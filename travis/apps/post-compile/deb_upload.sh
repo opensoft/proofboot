7088b256985430620345c188cb36916d606d1a5a
Denis Kormalev <denis.kormalev@opensoftdev.com>
2019-08-26 16:01:38 -0700

    Travis now can pack apps in docker images

--- travis/apps/post-compile/deb_upload.sh
+++ travis/apps/post-compile/deb_upload.sh
@@ -49,52 +49,7 @@ pip install --user awscli;
 travis_time_finish && travis_fold end "prepare.awscli";
 echo " ";
 
-travis_fold start "prepare.docker" && travis_time_start;
-echo -e "\033[1;33mDownloading and starting Docker container...\033[0m";
-docker pull opensoftdev/proof-builder-base:latest;
-docker run --privileged -id --name builder -w="/sandbox" -v $(pwd):/sandbox/target_src -v $HOME/full_build:/sandbox/build \
-    -v $HOME/proof-bin:/opt/Opensoft/proof -v $HOME/extra_s3_deps:/sandbox/extra_s3_deps \
-    -e "PACKAGE_ROOT=/sandbox/package-$TARGET_NAME" -e "TARGET_NAME=$TARGET_NAME" -e "PROOF_PATH=/opt/Opensoft/proof" \
-    opensoftdev/proof-builder-base tail -f /dev/null;
-docker ps;
-travis_time_finish && travis_fold end "prepare.docker";
-echo " ";
-
-travis_fold start "prepare.apt_cache" && travis_time_start;
-echo -e "\033[1;33mUpdating apt cache...\033[0m";
-docker exec -t builder apt-get update;
-travis_time_finish && travis_fold end "prepare.apt_cache";
-echo " ";
-
-if [ -n "$EXTRA_DEPS" ]; then
-    travis_fold start "prepare.extra_deps" && travis_time_start;
-    echo -e "\033[1;33mInstalling extra dependencies...\033[0m";
-    docker exec -t builder bash -c "apt-get -qq install $EXTRA_DEPS -y --no-install-recommends";
-    travis_time_finish && travis_fold end "prepare.extra_deps";
-    echo " ";
-fi
-
-if [ -n "$(ls -A $HOME/extra_s3_deps/*.deb 2>/dev/null)" ]; then
-    travis_fold start "prepare.extra_s3_deps" && travis_time_start;
-    echo -e "\033[1;33mInstalling extra dependencies downloaded from S3...\033[0m";
-    docker exec -t builder bash -c "(dpkg -i /sandbox/extra_s3_deps/*.deb 2> /dev/null || apt-get -qq -f install -y --no-install-recommends)";
-    travis_time_finish && travis_fold end "prepare.extra_s3_deps";
-    echo " ";
-fi
-
-travis_fold start "prepare.dirs" && travis_time_start;
-echo -e "\033[1;33mPreparing dirs structure...\033[0m";
-echo "$ cp build/package-$TARGET_NAME.tar.gz ./ && tar -xzf package-$TARGET_NAME.tar.gz";
-docker exec -t builder bash -c "cp build/package-$TARGET_NAME.tar.gz ./ && tar -xzf package-$TARGET_NAME.tar.gz";
-travis_time_finish && travis_fold end "prepare.dirs";
-echo " ";
-
-travis_fold start "pack.deb" && travis_time_start;
-echo -e "\033[1;33mCreating deb package...\033[0m";
-echo "$ /opt/Opensoft/proof/dev-tools/deploy/debian/build_deb_package.sh /sandbox/target_src";
-docker exec -t builder bash -c "/opt/Opensoft/proof/dev-tools/deploy/debian/build_deb_package.sh /sandbox/target_src";
-travis_time_finish && travis_fold end "pack.deb";
-echo " ";
+$HOME/proof-bin/dev-tools/travis/pack_app_deb_helper.sh
 
 DEB_FILENAME=`find -maxdepth 1 -name "$TARGET_NAME-*.deb" -exec basename "{}" \; -quit`
 if [ -z  "$DEB_FILENAME" ]; then
---
+++ travis/apps/post-compile/dockerize.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# Copyright 2018, OpenSoft Inc.
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without modification, are permitted
+# provided that the following conditions are met:
+
+#     * Redistributions of source code must retain the above copyright notice, this list of
+# conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright notice, this list of
+# conditions and the following disclaimer in the documentation and/or other materials provided
+# with the distribution.
+#     * Neither the name of OpenSoft Inc. nor the names of its contributors may be used to endorse
+# or promote products derived from this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Author: denis.kormalev@opensoftdev.com (Denis Kormalev)
+
+set -e
+
+APP_VERSION="$($HOME/proof-bin/dev-tools/travis/grep_proof_app_version.sh .)";
+
+$HOME/proof-bin/dev-tools/travis/pack_app_deb_helper.sh
+
+DEB_FILENAME=`find -maxdepth 1 -name "$TARGET_NAME-*.deb" -exec basename "{}" \; -quit`
+if [ -z "$DEB_FILENAME" ]; then
+    echo -e "\033[1;31mCan't find created deb package, halting\033[0m";
+    exit 1
+fi
+
+travis_time_start;
+echo -e "\033[1;33mCreating docker image...\033[0m";
+mkdir build && mv $DEB_FILENAME build/;
+
+if [ ! -f Dockerfile ]; then
+cat << EOT > Dockerfile
+FROM opensoftdev/proof-app-deploy-base
+COPY build/*.deb /build/
+RUN apt-get -qq update \
+    && if [ -n "$USE_OPENCV" ]; then (dpkg -i /prebuilt-extras/*opencv*.deb 2> /dev/null || apt-get -qq -f install -y --no-install-recommends); fi \
+    && cd /build && (dpkg -i *.deb 2> /dev/null || apt-get -qq -f install -y --no-install-recommends) \
+    && rm -rf /build && /image_cleaner.sh
+USER proof:proof
+VOLUME /home/proof/.config/Opensoft /home/proof/.local/share/Opensoft/prooflogs \
+/home/proof/.local/share/Opensoft/$TARGET_NAME /home/proof/.cache/Opensoft/$TARGET_NAME $EXTRA_DOCKER_VOLUMES
+ENTRYPOINT ["/opt/Opensoft/$TARGET_NAME/bin/$TARGET_NAME"]
+EOT
+fi
+
+echo -e "\033[1mDockerfile contents:\033[0m"
+cat Dockerfile;
+echo "";
+docker build -t opensoftdev/$TARGET_NAME -f Dockerfile ./;
+
+echo -e "\033[1;33mTagging docker images for upload from one built above:\033[0m";
+if [ -n "$TRAVIS_TAG" ]; then
+    echo "$DOCKER_REGISTRY/$TARGET_NAME:$APP_VERSION";
+    docker tag opensoftdev/$TARGET_NAME $DOCKER_REGISTRY/$TARGET_NAME:$APP_VERSION;
+    echo "$DOCKER_REGISTRY/$TARGET_NAME:latest";
+    docker tag opensoftdev/$TARGET_NAME $DOCKER_REGISTRY/$TARGET_NAME:latest;
+    echo "$DOCKER_REGISTRY/$TARGET_NAME-prod:$APP_VERSION";
+    docker tag opensoftdev/$TARGET_NAME $DOCKER_REGISTRY/$TARGET_NAME-prod:$APP_VERSION;
+    echo "$DOCKER_REGISTRY/$TARGET_NAME-prod:latest";
+    docker tag opensoftdev/$TARGET_NAME $DOCKER_REGISTRY/$TARGET_NAME-prod:latest;
+else
+    echo "$DOCKER_REGISTRY/$TARGET_NAME:$TRAVIS_BRANCH-$APP_VERSION";
+    docker tag opensoftdev/$TARGET_NAME $DOCKER_REGISTRY/$TARGET_NAME:$TRAVIS_BRANCH-$APP_VERSION;
+    echo "$DOCKER_REGISTRY/$TARGET_NAME:$TRAVIS_BRANCH-latest";
+    docker tag opensoftdev/$TARGET_NAME $DOCKER_REGISTRY/$TARGET_NAME:$TRAVIS_BRANCH-latest;
+fi
+travis_time_finish
+
+source $HOME/proof-bin/dev-tools/travis/detect_build_type.sh;
+if [ -z "$SKIP_UPLOAD" ]; then
+    travis_time_start;
+    echo -e "\033[1;33mPushing docker images to registry...\033[0m";
+    echo "$DOCKER_PASSWORD" | docker login -u $DOCKER_USERNAME --password-stdin $DOCKER_REGISTRY;
+    for img in `docker images --format "{{.Repository}}:{{.Tag}}" $DOCKER_REGISTRY/$TARGET_NAME* | grep -v "<none>"`; do
+        echo -e "\033[1mPushing $img...\033[0m";
+        docker push $img;
+    done
+    travis_time_finish;
+fi
